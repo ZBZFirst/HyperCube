15c9aaba362e467e4af7f51611cffc9229e09c8a
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 09:38:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ import { createCube } from './createCube.js';
 import { applyPropertiesToCube } from './changeCube.js';
 
 async function init() {
-    const { scene, camera, renderer } = createScene();
+    const { scene, camera, renderer, updateControls } = createScene();
+
 
     const data = await d3.csv("pubmed_data.csv");
     const cube = createCube();
-    applyPropertiesToCube(cube, data[0]);
     scene.add(cube);
 
     function animate() {
         requestAnimationFrame(animate);
         cube.rotation.y += 0.01;
+        const delta = clock.getDelta();
+        updateControls(delta);
         renderer.render(scene, camera);
     }
     animate();
