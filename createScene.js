b85bcf33aaba0a079e2da5f2c132338b1cd0e5c7
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 09:41:53 -0700

    Update createScene.js

--- createScene.js
+++ createScene.js
@@ -1,5 +1,4 @@
 import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { PointerLockControls } from 'https://unpkg.com/three@0.158.0/examples/jsm/controls/PointerLockControls.js';
 
 export function createScene() {
     const scene = new THREE.Scene();
