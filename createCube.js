a67b6d00a3e795d1c94c552208b84e49dd3316f6
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 09:29:33 -0700

    Update createCube.js

--- createCube.js
+++ createCube.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
+
 export function createCube(data) {
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ffcc });
