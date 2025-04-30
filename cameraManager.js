af51d336b3e18c20c29776dde995dc66de57e5a5
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 12:19:27 -0700

    Create cameraManager.js

---
+++ cameraManager.js
@@ -0,0 +1,17 @@
+// cameraManager.js
+import * as THREE from 'three';
+
+let camera;
+
+export function initCamera(cameraInstance) {
+    camera = cameraInstance;
+}
+
+export function centerCameraOnCube(cube) {
+    const targetPosition = cube.position.clone();
+    targetPosition.y += 1;
+    targetPosition.z += 5;
+
+    camera.position.lerp(targetPosition, 0.1);
+    camera.lookAt(cube.position);
+}
