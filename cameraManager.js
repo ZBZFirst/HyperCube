59cbb9c3e40a6b19d76238dfe4507dc5b14d8137
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 13:05:50 -0700

    Update cameraManager.js

--- cameraManager.js
+++ cameraManager.js
@@ -7,11 +7,5 @@ export function initCamera(cameraInstance) {
     camera = cameraInstance;
 }
 
-export function centerCameraOnCube(cube) {
-    const targetPosition = cube.position.clone();
-    targetPosition.y += 1;
-    targetPosition.z += 5;
-    camera.position.lerp(targetPosition, 0.1);
-    camera.lookAt(cube.position);
-}
+
 // cameraManager.js end
