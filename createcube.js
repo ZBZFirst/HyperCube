32572c161f67a3cef389df9eac3f227b5b4cd3e4
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-30 09:18:27 -0700

    Create createcube.js

---
+++ createcube.js
@@ -0,0 +1,8 @@
+export function createCube(data) {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ffcc });
+    const cube = new THREE.Mesh(geometry, material);
+
+    cube.userData = data;
+    return cube;
+}
