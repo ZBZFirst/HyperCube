89d85183d68e1f8b0a8414b5e078c40e10e099a8
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-05-05 19:14:38 -0700

    Update dataManager.js

--- dataManager.js
+++ dataManager.js
@@ -1,6 +1,6 @@
 // dataManager.js
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
-import { fetchPubMedData } from './pubmedFetcher.js';
+import { fetchPubMedData, DEFAULT_API_KEY } from './pubmedFetcher.js';
 import { showPubMedFetchOverlay, hidePubMedFetchOverlay } from './pubmedOverlay.js';
 
 let data = [];
