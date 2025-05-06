8f872d19f20ea5186f41b3e1662c5b026d37aa23
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-05-06 10:36:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ import { createCubesFromData, getCubes, highlightCubeByPmid, centerCameraOnCube,
 import { hidePubMedFetchOverlay } from './pubmedOverlay.js';
 import { deleteSelectedFromData } from './deleteCubes.js';
 import { exportFilteredData } from './saveCubes.js';
+import { setupScene } from './sceneSetup.js';
 
 let sceneObjects;
 let selectedCubes = [];
