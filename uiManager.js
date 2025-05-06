e6f635844e326d87b6b56ba9d9beee8afed0e802
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-05-05 19:06:37 -0700

    Update uiManager.js

--- uiManager.js
+++ uiManager.js
@@ -2,6 +2,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 import { loadData, exportFilteredData, populateDataTable, updateTextZone, attemptPubMedFetch, hidePubMedFetchOverlay, deleteFromData, getData, deleteSelectedFromData, addAnnotation } from './dataManager.js';
 import { createCubesFromData, deleteSelectedCubes, getCubes, highlightCubeByPmid, centerCameraOnCube, initCubeManager } from './cubeManager.js';
+import { hidePubMedFetchOverlay } from './pubmedOverlay.js';
 
 export function createUI(callbacks) {
     const uiContainer = d3.select('#data-container');
